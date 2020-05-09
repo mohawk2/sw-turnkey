d49f6c0f41e4c55799ea261616571368ad1bb388
Ed J <mohawk2@users.noreply.github.com>
2020-05-09 02:51:36 +0100

    fat arrows

--- serviceworker.js
+++ serviceworker.js
@@ -5,23 +5,19 @@
 
   function jsonCachingFetch(url) {
     var request = new Request(url);
-    return caches.match(request).then(function(cacheResponse) {
+    return caches.match(request).then(cacheResponse => {
       return cachingFetchOrCached(request, cacheResponse);
-    }).then(function(response) {
-      return response.json();
-    }).catch(function(e) {
+    }).then(response => response.json()).catch(e => {
       console.log('Error getting config:', e);
       throw e;
     });
   }
 
   function cachingFetch(request) {
-    return fetch(request).then(function (networkResponse) {
+    return fetch(request).then(networkResponse => {
       var nrClone = networkResponse.clone(); // capture here else extra ticks will make body be read by time get to inner .then
       if (networkResponse.ok) {
-        caches.open(cachename).then(function(cache) {
-          return cache.put(request, nrClone);
-        });
+        caches.open(cachename).then(cache => cache.put(request, nrClone));
       }
       return networkResponse;
     });
@@ -29,50 +25,45 @@
 
   function cachingFetchOrCached(request, cacheResponse) {
     var originalResponse;
-    return cachingFetch(request).then(function(response) {
+    return cachingFetch(request).then(response => {
       if (response.ok) return response;
       originalResponse = response;
       throw "Error";
-    }).catch(function(error) {
-      if (cacheResponse) return cacheResponse;
-      return originalResponse;
-    });
+    }).catch(error => cacheResponse || originalResponse);
   }
 
   function maybeMatch(configObj, key, value) {
     return configObj[key] && value.match(configObj[key].re);
   }
 
-  self.addEventListener("install", function(event) {
+  self.addEventListener("install", event => {
     console.log("Installing SW...");
     var configObj;
-    event.waitUntil(jsonCachingFetch(configURL).then(function(response) {
+    event.waitUntil(jsonCachingFetch(configURL).then(response => {
       configObj = response;
       return caches.open(cachename);
-    }).then(function(cache) {
+    }).then(cache => {
       console.log("Caching: ", configObj.precache_urls);
       return cache.addAll(configObj.precache_urls);
-    }).then(function() {
-      console.log("The SW is now installed");
-    }));
+    }).then(() => console.log("The SW is now installed")));
   });
 
-  self.addEventListener("fetch", function(event) {
-    event.respondWith(jsonCachingFetch(configURL).then(function(response) {
+  self.addEventListener("fetch", event => {
+    event.respondWith(jsonCachingFetch(configURL).then(response => {
       var configObj = response;
       if (maybeMatch(configObj, 'network_only', event.request.url)) {
-        return fetch(event.request).catch(function() {});
+        return fetch(event.request).catch(() => {});
       }
-      return caches.open(cachename).then(function(cache) {
-        return cache.match(event.request);
-      }).then(function(cacheResponse) {
+      return caches.open(cachename).then(
+        cache => cache.match(event.request)
+      ).then(cacheResponse => {
         if (cacheResponse && maybeMatch(configObj, 'cache_only', event.request.url)) {
           return cacheResponse;
         }
         if (maybeMatch(configObj, 'network_first', event.request.url)) {
           return cachingFetchOrCached(event.request, cacheResponse);
         }
-        return cacheResponse || cachingFetch(event.request).catch(function() {});
+        return cacheResponse || cachingFetch(event.request).catch(() => {});
       });
     }));
   });
